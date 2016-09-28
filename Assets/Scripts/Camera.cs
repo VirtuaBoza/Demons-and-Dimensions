585261c3204ae4400d010e27c1b2ebaa85ec0b85
Andrew Boza <andrew.m.boza@gmail.com>
2016-09-27 22:16:43 -0500

    Made Camera Follow Player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	private Player player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x,player.transform.position.y,transform.position.z);
+	}
+}
