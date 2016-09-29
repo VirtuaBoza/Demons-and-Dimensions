4bae86daf8d997d137df0b2ada56cefe44f6255e
Andrew Boza <andrew.m.boza@gmail.com>
2016-09-28 21:40:53 -0500

    Made fight scene kinda work

---
+++ Assets/Scripts/FightMenuButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FightMenuButton : MonoBehaviour {
+
+	private Text buttonText;
+
+	// Use this for initialization
+	void Start () {
+		buttonText = GetComponent<Text>();
+		Debug.Log(buttonText.text);
+	}
+	
+	public void EmboldenText(){
+		Debug.Log("This is supposed to make the text bold");
+		buttonText.fontStyle = FontStyle.Bold;
+	}
+
+	public void UnboldenText(){
+		Debug.Log("This is supposed to make the text bold");
+		buttonText.fontStyle = FontStyle.Normal;
+	}
+}
