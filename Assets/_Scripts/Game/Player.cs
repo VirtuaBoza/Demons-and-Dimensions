8ee9188a1072e8cb6f7ef08db843b291f2a461b1
Andrew Boza <andrew.m.boza@gmail.com>
2016-11-19 21:05:39 -0600

    Adds controller input for moving player

--- Assets/_Scripts/Game/Player.cs
+++ Assets/_Scripts/Game/Player.cs
@@ -14,28 +14,28 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("right")) {
+		if (Input.GetAxis("Horizontal") > 0) {
 			animator.SetBool ("isWalkingRight", true);
 			transform.Translate (Vector3.right * Time.deltaTime * playerSpeed, Space.World);
 		} else {
 			animator.SetBool ("isWalkingRight", false);
 		}
 
-		if (Input.GetKey("up")) {
+		if (Input.GetAxis("Vertical") > 0) {
 			animator.SetBool ("isWalkingUp", true);
 			transform.Translate (Vector3.up * Time.deltaTime * playerSpeed, Space.World);
 		} else {
 			animator.SetBool ("isWalkingUp", false);
 		}
 
-		if (Input.GetKey("left")) {
+		if (Input.GetAxis("Horizontal") < 0) {
 			animator.SetBool ("isWalkingLeft", true);
 			transform.Translate (Vector3.left * Time.deltaTime * playerSpeed, Space.World);
 		} else {
 			animator.SetBool ("isWalkingLeft", false);
 		}
 
-		if (Input.GetKey("down")) {
+		if (Input.GetAxis("Vertical") < 0) {
 			animator.SetBool ("isWalkingDown", true);
 			transform.Translate (Vector3.down * Time.deltaTime * playerSpeed, Space.World);
 		} else {
