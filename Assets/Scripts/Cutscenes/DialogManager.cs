8c7df7b5fb3804311d924275cad4f4512dcb8e13
VirtuaBoza <andrew.m.boza@gmail.com>
2016-11-30 11:43:48 -0600

    Adds dialog autotype feature

--- Assets/Scripts/Cutscenes/DialogManager.cs
+++ Assets/Scripts/Cutscenes/DialogManager.cs
@@ -7,9 +7,15 @@ public class DialogManager : MonoBehaviour {
 	public TextAsset textFile;
 	public string[] linesOfText;
 	public Text speaker, dialog;
+	public float letterPause = 0.05f;
+	public float sentencePause = 0.5f;
+	public AudioClip sound;
+	public bool soundEnabled = true;
+	public Image dialogArrow;
 
 	private int speakerIndex = 0;
 	private int dialogIndex = 1;
+	private bool isTyping = false; 
 
 	// Use this for initialization
 	void Start () {
@@ -17,23 +23,58 @@ public class DialogManager : MonoBehaviour {
 			linesOfText = (textFile.text.Split('\n'));
 			UpdateText();
 		}
+		dialogArrow.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.RightArrow) && dialogIndex + 1 <= linesOfText.Length){
-			speakerIndex += 2;
-			dialogIndex += 2;
-			UpdateText();
+		if (Input.GetKeyDown(KeyCode.Space) && dialogIndex + 1 <= linesOfText.Length){
+			UpdateText ();
 		}
-
+			
 	}
 
-	void UpdateText () {
-		speaker.text = linesOfText[speakerIndex];
-		dialog.text = linesOfText[dialogIndex];
-		ColorSpeakerText(linesOfText[speakerIndex]);
+	public void UpdateText () {
+		if(!isTyping){
+			isTyping = true;
+			speakerIndex += 2;
+			dialogIndex += 2;
+			speaker.text = linesOfText[speakerIndex];
+			ColorSpeakerText(linesOfText[speakerIndex]);
+			dialog.text = "";
+			StartCoroutine(TypeText());
+			dialogArrow.gameObject.SetActive (false);
+		} else {
+			FinishTyping ();
+		}
+	}
+
+	IEnumerator TypeText() {
+		int charPosition = 0;
+		foreach (char letter in linesOfText[dialogIndex].ToCharArray()){
+			dialog.text += letter;
+			if (sound && soundEnabled) {
+				GetComponent<AudioSource> ().PlayOneShot (sound);
+			}
+			charPosition++;
+			if (charPosition == linesOfText[dialogIndex].ToCharArray().Length){
+				Debug.Log ("Reached last character");
+				FinishTyping ();
+			}
+			if(letter == '.' || letter == '?') {
+				yield return new WaitForSeconds (sentencePause);
+			} else {
+				yield return new WaitForSeconds (letterPause);
+			}
+		}
+	}
+
+	void FinishTyping() {
+		StopAllCoroutines();
+		isTyping = false;
+		dialog.text = linesOfText [dialogIndex];
+		dialogArrow.gameObject.SetActive (true);
 	}
 
 	void ColorSpeakerText (string speaker) {
