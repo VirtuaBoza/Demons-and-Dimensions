4212fdcadb382a9fb79e83a95fc9b301118a5a69
Andrew Boza <andrew.m.boza@gmail.com>
2016-12-05 21:38:49 -0600

    Refixes equip toggles

--- Assets/Menus/Scripts/EquipPanel.cs
+++ Assets/Menus/Scripts/EquipPanel.cs
@@ -11,12 +11,12 @@ public class EquipPanel : MonoBehaviour {
 	public GameObject[] characterPanels; 
 	public CHARACTER currentCharacter;
 	public Toggle blueToggle, orangeToggle, greenToggle, redToggle;
-	public Sprite brightBlueBubble, brightOrangeBubble, brightGreenBubble, brightRedBubble,
-	dullBlueBubble, dullOrangeBubble, dullGreenBubble, dullRedBubble;
 
 	void Start () {
 		characterPanels = new GameObject[] { crystalPanel, teddyPanel, hunterPanel, damienPanel};
-		SwitchCharacter ((int)currentCharacter);
+		Toggle[] toggles = new Toggle[] {blueToggle, orangeToggle, greenToggle, redToggle};
+		toggles[(int)currentCharacter].isOn = true;
+		//SwitchCharacter ((int)currentCharacter);
 	}
 
 	public void SwitchCharacter (int index) {
@@ -25,25 +25,21 @@ public class EquipPanel : MonoBehaviour {
 			currentCharacter = CHARACTER.Crystal;
 			ShowAppropriatePanel(crystalPanel);
 			nameFrameText.text = "Crystal";
-			IlluminateToggle (blueToggle);
 			break;
 		case 1:
 			currentCharacter = CHARACTER.Teddy;
 			ShowAppropriatePanel(teddyPanel);
 			nameFrameText.text = "Teddy";
-			IlluminateToggle (orangeToggle);
 			break;
 		case 2:
 			currentCharacter = CHARACTER.Hunter;
 			ShowAppropriatePanel(hunterPanel);
 			nameFrameText.text = "Hunter";
-			IlluminateToggle (greenToggle);
 			break;
 		case 3:
 			currentCharacter = CHARACTER.Damien;
 			ShowAppropriatePanel(damienPanel);
 			nameFrameText.text = "Damien";
-			IlluminateToggle (redToggle);
 			break;
 		}
 	}
@@ -55,34 +51,6 @@ public class EquipPanel : MonoBehaviour {
 			} else {
 				panel.SetActive (false);
 			}
-		}
-	}
-
-	public void IlluminateToggle (Toggle toggle) {
-		if (toggle == blueToggle) {
-			blueToggle.GetComponent<Image>().sprite = brightBlueBubble;
-
-			orangeToggle.GetComponent<Image>().sprite = dullOrangeBubble;
-			greenToggle.GetComponent<Image>().sprite = dullGreenBubble;
-			redToggle.GetComponent<Image>().sprite = dullRedBubble;
-		} else if (toggle == orangeToggle) {
-			orangeToggle.GetComponent<Image>().sprite = brightOrangeBubble;
-
-			blueToggle.GetComponent<Image>().sprite = dullBlueBubble;
-			greenToggle.GetComponent<Image>().sprite = dullGreenBubble;
-			redToggle.GetComponent<Image>().sprite = dullRedBubble;
-		} else if (toggle == greenToggle) {
-			greenToggle.GetComponent<Image>().sprite = brightGreenBubble;
-
-			orangeToggle.GetComponent<Image>().sprite = dullOrangeBubble;
-			blueToggle.GetComponent<Image>().sprite = dullBlueBubble;
-			redToggle.GetComponent<Image>().sprite = dullRedBubble;
-		} else if (toggle == redToggle) {
-			redToggle.GetComponent<Image>().sprite = brightRedBubble;
-
-			orangeToggle.GetComponent<Image>().sprite = dullOrangeBubble;
-			greenToggle.GetComponent<Image>().sprite = dullGreenBubble;
-			blueToggle.GetComponent<Image>().sprite = dullBlueBubble;
 		}
 	}
 
