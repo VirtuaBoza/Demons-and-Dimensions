ea1bc16c54ce02eae5446a22c9663cf844dfeda8
Andrew Boza <andrew.m.boza@gmail.com>
2016-12-10 22:13:07 -0600

    Reorganizes assets and adds characterdatabase.cs

---
+++ Assets/Scripts/Game/BaseCharacter.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum AbilityName {Str,Dex,Con,Int,Wis,Cha}
+
+public class AbilityScore {
+	public int BaseValue {get;set;}
+	public bool Proficient {get;set;}
+	public AbilityScore () {
+		BaseValue = 0;
+		Proficient = false;
+	}
+}
+
+public struct ModifyingSkill {
+	public AbilityScore skill;
+	public int change;
+}
+
+public class BaseCharacter : MonoBehaviour {
+
+	public string characterName;
+	public int lvl;
+	public int xp;
+	public int profBonus;
+
+	private AbilityScore[] skills;
+
+	void Awake() {
+		characterName = string.Empty;
+		lvl = 1;
+		xp = 0;
+		profBonus = 2;
+		skills = new AbilityScore[Enum.GetValues(typeof(AbilityName)).Length];
+	}
+
+	public void AddXp (int amount) {
+		xp += amount;
+		CalculateLevelAndProfBonus();
+	}
+
+	public void CalculateLevelAndProfBonus () {
+		if (xp < 300) {lvl = 1;}
+		else if (xp < 900) {lvl = 2;}
+		else if (xp < 2700) {lvl = 3;}
+		else if (xp < 6500) {lvl = 4;}
+		else if (xp < 14000) {lvl = 5;}
+		else if (xp < 23000) {lvl = 6;}
+		else if (xp < 34000) {lvl = 7;}
+		else if (xp < 48000) {lvl = 8;}
+		else if (xp < 64000) {lvl = 9;}
+		else if (xp < 85000) {lvl = 10;}
+		else if (xp < 100000) {lvl = 11;}
+		else if (xp < 120000) {lvl = 12;}
+		else if (xp < 140000) {lvl = 13;}
+		else if (xp < 165000) {lvl = 14;}
+		else if (xp < 195000) {lvl = 15;}
+		else if (xp < 225000) {lvl = 16;}
+		else if (xp < 265000) {lvl = 17;}
+		else if (xp < 305000) {lvl = 18;}
+		else if (xp < 355000) {lvl = 19;}
+		else {lvl = 20;}
+
+		if (lvl < 5) {profBonus = 2;}
+		else if (lvl < 9) {profBonus = 3;}
+		else if (lvl < 13) {profBonus = 4;}
+		else if (lvl < 17) {profBonus = 5;}
+		else {profBonus = 6;}
+	}
+
+	private void SetupSkills() {
+		for (int i = 0; i < skills.Length; i++) {
+			skills[i] = new AbilityScore();
+		}
+	}
+
+}
---
+++ Assets/Scripts/Game/CharacterDatabase.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+
+public class CharacterDatabase : MonoBehaviour {
+
+	private List<Character> characterDatabase = new List<Character>();
+	private JsonData characterData;
+
+	// Use this for initialization
+	void Start () {
+		characterData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Characters.json"));
+		ConstructCharacterDatabase();
+	}
+
+	private void ConstructCharacterDatabase() {
+		for (int i = 0; i < characterData.Count; i++) {
+			List<int> list = new List<int>();
+			int itemCnt = characterData[i]["startingequipment"].Count;
+			for (int n = 0; n < itemCnt; n++) {
+				list.Add((int)characterData[i]["startingequipment"][n]);
+			}
+			characterDatabase.Add(new Character((int)characterData[i]["id"],
+				characterData[i]["name"].ToString(),
+				characterData[i]["class"].ToString(),
+				characterData[i]["race"].ToString(),
+				(int)characterData[i]["speed"],
+				(int)characterData[i]["basehp"],
+				(int)characterData[i]["hitdice"],
+				(int)characterData[i]["baseabilityscores"]["str"],
+				(int)characterData[i]["baseabilityscores"]["dex"],
+				(int)characterData[i]["baseabilityscores"]["con"],
+				(int)characterData[i]["baseabilityscores"]["int"],
+				(int)characterData[i]["baseabilityscores"]["wis"],
+				(int)characterData[i]["baseabilityscores"]["cha"],
+				(bool)characterData[i]["throwprofs"]["str"],
+				(bool)characterData[i]["throwprofs"]["dex"],
+				(bool)characterData[i]["throwprofs"]["con"],
+				(bool)characterData[i]["throwprofs"]["int"],
+				(bool)characterData[i]["throwprofs"]["wis"],
+				(bool)characterData[i]["throwprofs"]["cha"],
+				list));
+		}
+	}
+	
+}
+
+public class Character {
+
+	public int Id {	get;set;}
+	public string Name {get;set;}
+	public string Class {get;set;}
+	public string Race {get;set;}
+	public int Speed {get;set;}
+	public int BaseHp {get;set;}
+	public int HitDice {get;set;}
+	public int StrScore {get;set;}
+	public int DexScore {get;set;}
+	public int ConScore {get;set;}
+	public int IntScore {get;set;}
+	public int WisScore {get;set;}
+	public int ChaScore {get;set;}
+	public bool StrProf {get;set;}
+	public bool DexProf {get;set;}
+	public bool ConProf {get;set;}
+	public bool IntProf {get;set;}
+	public bool WisProf {get;set;}
+	public bool ChaProf {get;set;}
+	public List<int> StartingEquipment {get;set;}
+
+	public Character (int id, string cname, string cclass, string race, int speed, int basehp, int hitdice,
+		int strscore, int dexscore, int conscore, int intscore, int wisscore, int chascore,
+		bool strprof, bool dexprof, bool conprof, bool intprof, bool wisprof, bool chaprof, List<int> startingequipment) {
+		this.Id = id;
+		this.Name = cname;
+		this.Class = cclass;
+		this.Race = race;
+		this.Speed = speed;
+		this.BaseHp = basehp;
+		this.HitDice = hitdice;
+		this.StrScore = strscore;
+		this.DexScore = dexscore;
+		this.ConScore = conscore;
+		this.IntScore = intscore;
+		this.WisScore = wisscore;
+		this.ChaScore = chascore;
+		this.StrProf = strprof;
+		this.DexProf = dexprof;
+		this.ConProf = conprof;
+		this.IntProf = intprof;
+		this.WisProf = wisprof;
+		this.ChaProf = chaprof;
+		this.StartingEquipment = startingequipment;
+	}
+}
