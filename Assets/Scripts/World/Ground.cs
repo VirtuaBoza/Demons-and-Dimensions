96f36fc63dca4858a4b8541767bf3d064759266f
Andrew Boza <andrew.m.boza@gmail.com>
2017-12-02 21:06:56 -0500

    Adds Physics2DRaycaster to World Camera

--- Assets/Scripts/World/Ground.cs
+++ Assets/Scripts/World/Ground.cs
@@ -17,8 +17,6 @@ public class Ground : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        //Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
-        Debug.Log(name + " Game Object Clicked!");
         Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         target.z = player.transform.position.z;
         player.MovePlayer(target);
